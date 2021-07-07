9fc8cd75ea93f6250397797ffafbfedead100767
Anjo <350anviera195@gmail.com>
2021-07-07 14:32:00 +0800

    Update About page.

--- src/views/About.vue
+++ src/views/About.vue
@@ -9,7 +9,7 @@
 						<h2 class="text-center text-tapa-700 pb-10 px-10">
 							About Me
 						</h2>
-						I'm a self-taught web developer with 4 years of experience in making web applications using HTML, CSS, PHP, MySQL, and Javascript. After I graduated as an I.T student in college in 2016, I got a job right away as an I.T staff. I also have intermediate knowledge of computer troubleshooting and networking. I really have an interest and I truly enjoy web development, every day I spend my time learning and discover new technologies about web development from front end to back end. In January 2020 I decided to use Laravel, Laravel Livewire, and Vue JS in my future projects and I also have experience in other tools such as Python, Django, and React JS.
+						I'm a self-taught web developer with experience in making web applications using HTML, CSS, PHP, MySQL, and Javascript.
 					</div>
 					<div class="absolute -mt-14 bg-tapa-900 p-16 w-full transform skew-y-3 bg-gradient-to-r from-tapa-500 to-tapa-700"></div>
 				</div>
